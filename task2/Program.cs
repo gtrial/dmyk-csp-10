4eb2d6f28c3044ec6c2f1294372d20484eacad92
Dmytro Kostiuchenko <dmyk@nuuday.dk>
2021-07-02 17:58:55 +0300

    Task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -6,7 +6,26 @@ namespace task2
     {
         private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var myClass1 = new MyClass1();
+            myClass1.Method1();
+            var myClass0 = (MyClass0) myClass1;
+            myClass0.Method1();
+        }
+    }
+
+    internal class MyClass0
+    {
+        public virtual void Method1()
+        {
+            Console.WriteLine("MyClass0.Method1");
+        }
+    }
+
+    internal class MyClass1 : MyClass0
+    {
+        public override void Method1()
+        {
+            Console.WriteLine("MyClass1.Method1");
         }
     }
 }
